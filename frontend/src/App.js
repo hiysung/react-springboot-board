56876a4ec4ff1e529c7dba2273c9b9baebe44174
HyeSeong Choi <hiysung@naver.com>
2022-03-04 22:22:22 +0900

    front back rest API test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/home").then(response => response.text()).then(message => {
+      console.log(message);
+      setMessage(message);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +17,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{message}</h1>
       </header>
     </div>
   );
